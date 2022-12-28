aa12f4e3e345b396b1d8a6ef4a827852cd0bd3d4
Pedro <pedro@calasblasco.com>
2022-12-28 00:05:09 -0300

    feat mapa bases

--- distritos/js/map.js
+++ distritos/js/map.js
@@ -1,4 +1,3 @@
-
 //Obtengo los parametros y creo el objeto de parametros
 
 const values = window.location.search;
@@ -9,7 +8,6 @@ let params = {}
 for (const [key, value] of urlParams.entries()) {    
     params[key] = value
 }
-
 
 
 //repryección del mapa
@@ -30,50 +28,152 @@ proj4.defs(
 );
 
 
-console.log(params)
+//Mapa Base
+
+if(params.layerBase){
+    if(params.layerBase == "osm"){
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'OpenStreetMap'}).addTo(map);
+    }
+    else if(params.layerBase == "satelite"){
+        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {attribution: 'Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',ext: 'jpg',}).addTo(map)
+    }
+    else if(params.layerBase == "dark"){  
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {attribution: '{attribution.OpenStreetMap} &copy; <a href="https://carto.com/attributions">CARTO</a>',ext: 'jpg',}).addTo(map);
+    }
+    else if(params.layerBase == "light"){  
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {attribution: '{attribution.OpenStreetMap} &copy; <a href="https://carto.com/attributions">CARTO</a>',ext: 'jpg',}).addTo(map);
+    }
 
 
-//capas wms
+   
+    delete params.layerBase
+}
 
-var distritos = L.tileLayer.wms(distritosWms, {
-	layers: "distrito",
-	format: 'image/jpeg',
-	transparent: true,
-    version: '1.1.0',
+
+//separo mws layers de wfs
+wmsLayers = []
+wfsLayers = []
+
+for(const [key, value] of Object.entries(params)){
+    const arrayParams = value.split("-")
+    console.log(arrayParams)
+    arrayParams.map((item) =>{
+        if(item.split(":")[1] == "wms"){
+            wmsLayers.push(value)
+        }
+        else if(item.split(":")[1] == "wfs"){
+            wfsLayers.push(value)
+        }
+    })
+  }
+
+
+//cargar layers wms al mapa
+
+wmsLayers.map((item) => {
+
+    const wmsParams = item.split("-")
+
+    let version, format, layer, transparency
+
+    wmsParams.map((it) => {
+        if(it.split(":")[0] == "versionWms"){
+            version = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "formatWms"){
+            format = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "layerWms"){
+            layer = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "transparency"){
+            transparency = it.split(":")[1] 
+        }
+    })
+
+    L.tileLayer.wms(distritosWms, {
+	layers: layer,
+	format: format,
+	transparent: Boolean(transparency),
+    version: version,
     maxZoom: 14,
     minZoom: 12,
-    
-  
-}).addTo(map);
+    }).addTo(map);
+
+})
+
 
 
 //capas wfs
 
-let distritosWms = "https://geoserver.santafeciudad.gov.ar/geoserver/sitmax/wms?service=WMS&"
-
-let oficinas = "https://geoserver.santafeciudad.gov.ar/geoserver/sitmax/wfs?version=1.1.0&service=wfs&request=GetFeature&typeName=sitmax:oficinas_de_distritos&outputFormat=json"
+let oficinass = "https://geoserver.santafeciudad.gov.ar/geoserver/sitmax/wfs?version=1.1.0&service=wfs&request=GetFeature&typeName=sitmax:oficinas_de_distritos&outputFormat=json"
 
 let oficinas2 = `https://geoserver.santafeciudad.gov.ar/geoserver/sitmax/${params.service}?version=${params.version}&service=wfs&
 request=${params.request}&typeName=sitmax:${params.typeName}&outputFormat=${params.outputFormat} `
 
 
-fetch(oficinas)
-.then((res) => res.json())
-.then((data) => {
-    layer = L.Proj.geoJson(data, {
-        pointToLayer: function (feature, latlng) { 
-            return L.circleMarker(latlng, {
-                radius: 5,
-                fillColor: "red",
-                color: "red"
-            });
-        },
-        onEachFeature: function (feature, layer) {
-            return layer.bindPopup(` <h5>${feature.properties.name}</h5><hr><p>${feature.properties.dirección}</p> 
-               <p>${feature.properties.descriptio} hs</p><p>${feature.properties.caja} hs</p>
-               <p>${feature.properties["e-mail"]} hs</p>
-               <p>Teléfono: ${feature.properties.teléfono}</p>`);
-        },
-    }).addTo(map);
+
+wfsLayers.map((item) => {
+    
+    const wfsParams = item.split("-")
+    let service, version, request, typeName, outputFormat, color, geometry
+
+    console.log(wfsParams)
+
+    wfsParams.map((it) => {
+        if(it.split(":")[0] == "version"){
+            version = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "service"){
+            service = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "request"){
+            request = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "typeName"){
+            typeName = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "outputFormat"){
+            outputFormat = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "color"){
+            color = it.split(":")[1] 
+        }
+        else if(it.split(":")[0] == "geometry"){
+            geometry = it.split(":")[1] 
+        }
     })
-.catch((err) => console.error(err));
+
+    color = "#" + color
+
+    let url = `https://geoserver.santafeciudad.gov.ar/geoserver/sitmax/${service}?version=${version}&service=wfs&request=${request}&typeName=sitmax:${typeName}&outputFormat=${outputFormat} `
+    
+    console.log(oficinass)
+
+    console.log(url)
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+
+        console.log(data);
+
+        layer = L.Proj.geoJson(data, {
+            pointToLayer: function (feature, latlng) { 
+                return L.circleMarker(latlng, {
+                    radius: 5,
+                    fillColor: color,
+                    color: color
+                });
+            },
+            // onEachFeature: function (feature, layer) {
+            //     return layer.bindPopup(` <h5>${feature.properties.name}</h5><hr><p>${feature.properties.dirección}</p> 
+            //     <p>${feature.properties.descriptio} hs</p><p>${feature.properties.caja} hs</p>
+            //     <p>${feature.properties["e-mail"]} hs</p>
+            //     <p>Teléfono: ${feature.properties.teléfono}</p>`);
+            // },
+        }).addTo(map);
+    })
+    .catch((err) => console.error(err));
+})
+
--- distritos/js/selects.js
+++ distritos/js/selects.js
@@ -3,25 +3,71 @@ const selectService = document.querySelector("#selectService")
 const selectsWfsContainer = document.querySelector("#selectsWfsContainer")
 const selectsWmsContainer = document.querySelector("#selectsWmsContainer")
 
-//selects wfs
+const baseSelect = document.querySelector("#baseSelect")
+const selectMapContainer = document.querySelector("#selectMapContainer")
+const mapBaseButton = document.querySelector("#mapBaseButton")
+
+//selects wfs constants
 const selectFormat = document.querySelector("#selectFormat")
 const selectGeometry = document.querySelector("#selectGeometry")
 const selectColor = document.querySelector("#selectColor")
 const inputVersion = document.querySelector("#inputVersion")
 const inputType = document.querySelector("#inputType")
 const selectRequest = document.querySelector("#selectRequest")
+const wfsButton = document.querySelector("#wfsButton")
 
-//selects wms
+//selects wms constants
 const selectFormatWms = document.querySelector("#selectFormatWms")
 const inputLayerWms = document.querySelector("#inputLayerWms")
 const selectTransparency = document.querySelector("#selectTransparency")
 const inputVersionWms = document.querySelector("#inputVersionWms")
 
-
+//table costants
 const bodyTable = document.querySelector("#bodyTable")
-const goToMap = document.querySelector("#goToMap")
+const wmsButton = document.querySelector("#wmsButton")
 
 
+
+function onSelectBase(){
+    if(baseSelect.value =="true"){
+        selectMapContainer.style.display = "block"
+    }
+    if(baseSelect.value == "false"){
+        selectMapContainer.style.display = "none"
+    }
+    if(!baseSelect.value){
+        selectMapContainer.style.display = "none"
+    }
+}
+
+let contadorMapas = 0
+
+function loadMapBase(){
+    if(selectMap.value == "osm"){
+        contadorMapas++
+        params += "layerBase=osm&"
+        bodyTable.innerHTML += `<tr><th>Open Street Map</th><th></th><th class="text-center"><button class="btn btn-sm btn-danger"><i class="bi-trash"></i><button></th></tr>`
+    }
+    if(selectMap.value == "satelite"){
+        contadorMapas++
+        params += "layerBase=satelite&"
+        bodyTable.innerHTML += `<tr><th>Satélite</th><th></th><th class="text-center"><button class="btn btn-sm btn-danger"><i class="bi-trash"></i><button></th></tr>`
+    }
+    if(selectMap.value == "dark"){
+        contadorMapas++
+        params += "layerBase=dark&"
+        bodyTable.innerHTML += `<tr><th>Dark</th><th></th><th class="text-center"><button class="btn btn-sm btn-danger"><i class="bi-trash"></i><button></th></tr>`
+    }
+    if(selectMap.value == "light"){
+        contadorMapas++
+        params += "layerBase=light&"
+        bodyTable.innerHTML += `<tr><th>Light</th><th></th><th class="text-center"><button class="btn btn-sm btn-danger"><i class="bi-trash"></i><button></th></tr>`
+    }
+
+    if(contadorMapas >= 1){
+        mapBaseButton.disabled = true
+    }
+}
 
 
 function onSelectService(){
@@ -41,25 +87,23 @@ function onSelectService(){
 
 
 let params = ""
+let contadorLayers = 0
 
 function loadNewLayer(){
 
 
     if(selectService.value == "wfs"){
-        params += `version=${inputVersion.value}&service=wfs
-            &request=${selectRequest.value}&typeName=${inputType.value}&outputFormat=${selectFormat.value}
-            &color=${selectColor.value}&geometry=${selectGeometry.value}`
+        const color = selectColor.value.slice(1)
+        contadorLayers++
+        params += `layer${contadorLayers}=version:${inputVersion.value}-service:wfs-request:${selectRequest.value}-typeName:${inputType.value}-outputFormat:${selectFormat.value}-color:${color}-geometry:${selectGeometry.value}&`
 
-        bodyTable.innerHTML += `<tr><td>${inputType.value}</td><td>WFS</td><td><button class="btn btn-info">Delete<button></td></tr>`
-        console.log(bodyTable)
+        bodyTable.innerHTML += `<tr><td>${inputType.value}</td><td>WFS</td><td class="text-center"><button class="btn btn-sm btn-danger"><i class="bi-trash"></i><button></td></tr>`    
     }
 
     if(selectService.value == "wms"){
-        params += `versionWms=${inputVersionWms.value}&serviceWms=wms
-            &formatWms=${selectFormatWms.value}&layerWms=${inputLayerWms.value}&transparency=${selectTransparency.value}`
-
-        bodyTable.innerHTML += `<tr><td>${inputLayerWms.value}</td><td>WMS</td><td><button class="btn btn-info">Delete<button></td></tr>`
-        console.log(bodyTable)
+        contadorLayers++
+        params += `layer${contadorLayers}=versionWms:${inputVersionWms.value}-serviceWms:wms-formatWms:${selectFormatWms.value}-layerWms:${inputLayerWms.value}-transparency:${selectTransparency.value}&`
+        bodyTable.innerHTML += `<tr><td>${inputLayerWms.value}</td><td>WMS</td><td class="text-center"><button class="btn btn-sm btn-danger"><i class="bi-trash"></i><button></td></tr>`
     }
 
     selectsWfsContainer.style.display = "none"
@@ -67,17 +111,15 @@ function loadNewLayer(){
     
     selectService.value == ""
 
-    console.log(params)    
-
-    goToMap.setAttribute('href', `distritos/map.html?${params}`);
-
+    if(contadorLayers >= 4){
+        wfsButton.disabled = true;
+        wmsButton.disabled = true;
+    }
 
 }
 
+function goToMap(){
+    window.location.href = `distritos/map.html?${params}`
+}
 
-{/* <tr>
-                        
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                      </tr> */}
+
